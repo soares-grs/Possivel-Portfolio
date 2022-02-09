c1d5a704601b7ccb549d2894343f773f3c0ece17
Gabriel <gabrielrs.job@gmail.com>
2022-02-09 18:28:30 -0300

    Modal adicionado

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function animeScroll() {
 window.addEventListener('scroll', function(){
     animeScroll();
 })
+
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const closeButton = document.querySelector('.popup-close');
+
+button.addEventListener('click', () => {
+    popup.style.display = "block";
+})
+
+closeButton.addEventListener('click', () => {
+    popup.style.display = "none";
+})
