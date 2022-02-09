15052741f73340cffd41a450425d08cef47153dd
Gabriel <gabrielrs.job@gmail.com>
2022-02-08 23:29:08 -0300

    Setor de Skills feito

---
+++ script.js
@@ -0,0 +1,16 @@
+const target = document.querySelectorAll('[data-anime');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + 500;
+    target.forEach(function(element){
+        if(windowTop > element.offsetTop){
+            element.classList.add(animationClass);
+        }
+        
+    })
+}
+
+window.addEventListener('scroll', function(){
+    animeScroll();
+})
