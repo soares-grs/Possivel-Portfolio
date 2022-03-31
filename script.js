49aaf52e511da4f7eb7178ce90153e754d006d4e
Gabriel <gabrielrs.job@gmail.com>
2022-03-31 17:36:25 -0300

    Animações adicionadas

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function animeScroll() {
         if(windowTop > element.offsetTop){
             element.classList.add(animationClass);
         }
-        
     })
 }
 
@@ -15,14 +14,3 @@ window.addEventListener('scroll', function(){
     animeScroll();
 })
 
-const button = document.querySelector('button');
-const popup = document.querySelector('.popup-wrapper');
-const closeButton = document.querySelector('.popup-close');
-
-button.addEventListener('click', () => {
-    popup.style.display = "block";
-})
-
-closeButton.addEventListener('click', () => {
-    popup.style.display = "none";
-})
